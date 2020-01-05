38a5b05b1aa78af182cbbf9f6bd4de91629771be
kazu19970516 <58506497+kazu19970516@users.noreply.github.com>
2020-01-05 23:59:29 +0900

    u

--- js/main.js
+++ js/main.js
@@ -78,25 +78,20 @@ function work_back(){
   if(window.Worker){
       var worker = new Worker('js/worker.js');
   }
-  if(plancount == 4){
-    var root = []
-    root[0] = 4
-    root[1] = 8
-    root[2] = 6
-    ai1(root)
-    plancount = 5
-  }else if(plancount == 3){
-    var root = []
-    root[0] = 4
-    root[1] = 6
-    root[2] = 6
-    ai1(root)
-    plancount = 4
-  }else if(plancount == 0 || plancount == 1 || plancount == 2){
-    var root = []
-    root[0] = 0
-    ai1(root)
-    plancount++;
+  if(blue_wall == 0){
+    if(yellow_wall == 0){
+      work(wall,b1,b2,w1,w2,0)
+      var shortB = call_short(1)
+      var shortY = call_short(2)
+      if(shortB > shortY) victory = PIECE_TYPE.YELLOW
+      else victory = PIECE_TYPE.BLUE
+      show_result(victory);
+    }else{
+      var root1 = []
+      work(wall,b1,b2,w1,w2,2)
+      root1 = call()
+      ai1(root1)
+    }
   }else{
     console.log("緑色計算中")
     startTime = performance.now();
@@ -104,20 +99,25 @@ function work_back(){
                         "b1":b1,"b2":b2,"w1":w1,"w2":w2,
                         "turn":turn,"blue_wall":blue_wall,"yellow_wall":yellow_wall})
   }
-  // else if(blue_wall == 0){
-  //   if(yellow_wall == 0){
-  //     work(wall,b1,b2,w1,w2,0)
-  //     var shortB = call_short(1)
-  //     var shortY = call_short(2)
-  //     if(shortB > shortY) victory = PIECE_TYPE.YELLOW
-  //     else victory = PIECE_TYPE.BLUE
-  //     show_result(victory);
-  //   }else{
-  //     var root1 = []
-  //     work(wall,b1,b2,w1,w2,2)
-  //     root1 = call()
-  //     ai1(root1)
-  //   }
+  // if(plancount == 4){
+  //   var root = []
+  //   root[0] = 4
+  //   root[1] = 8
+  //   root[2] = 6
+  //   ai1(root)
+  //   plancount = 5
+  // }else if(plancount == 3){
+  //   var root = []
+  //   root[0] = 4
+  //   root[1] = 6
+  //   root[2] = 6
+  //   ai1(root)
+  //   plancount = 4
+  // }else if(plancount == 0 || plancount == 1 || plancount == 2){
+  //   var root = []
+  //   root[0] = 0
+  //   ai1(root)
+  //   plancount++;
   // }
   // if(plancount == 4){
   //   var root = []
